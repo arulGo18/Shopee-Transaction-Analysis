5a9412d46bb0ab8c340af1cb1307b005af05ee28
arulGo18 <ilyasyasinnurulah18@gmail.com>
2026-02-16 21:50:11 +0700

    Delete shopee-transaction-analysis directory

--- shopee-transaction-analysis/SQL/basic_analysis.sql
+++
@@ -1,7 +0,0 @@
--- Analisis dasar transaksi event vs hari biasa
-SELECT
-    jenis_hari,
-    COUNT(*) AS jumlah_transaksi,
-    ROUND(AVG(nilai_transaksi), 0) AS rata_rata_transaksi
-FROM transaksi
-GROUP BY jenis_hari;
